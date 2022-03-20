8fd5090e560ade8e8183c85fde3dd4fabced756c
Aditya Banik <90348622+Hackathon-2021-stack@users.noreply.github.com>
2022-03-20 11:08:50 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,7 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Component {...pageProps} />
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8895588978570093"
-     crossorigin="anonymous"></script>
-)
+  return <Component {...pageProps} />
 }
 
 export default MyApp
