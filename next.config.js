3fefbdff4dbd2376b69642823429c1b870b42b53
Aditya Banik <90348622+Hackathon-2021-stack@users.noreply.github.com>
2022-03-25 10:44:45 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images:{
-      domains:["ibb.co","i.ibb.co","image.tmdb.org","pngset.com"]
+      domains:["ibb.co","i.ibb.co","image.tmdb.org","pngset.com","raw.githubusercontent.com"]
     }
 }
